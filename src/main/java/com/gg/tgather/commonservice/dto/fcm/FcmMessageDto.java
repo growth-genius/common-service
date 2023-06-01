261f10a7b79008b5b90fd8954b3d6fb6bae8b1cd
leeseunggu <leesg6107@gmail.com>
2023-06-01 16:39:50 +0900

    feat: add fcmMessageDto

---
+++ src/main/java/com/gg/tgather/commonservice/dto/fcm/FcmMessageDto.java
@@ -0,0 +1,12 @@
+package com.gg.tgather.commonservice.dto.fcm;
+
+import lombok.Data;
+
+@Data
+public class FcmMessageDto {
+
+    private String title;
+    private String message;
+    private String image;
+
+}
