f94a32d697c67ed80dee284c430b6b72f7a8b1bc
leeseunggu <leesg6107@gmail.com>
2023-06-03 19:20:49 +0900

    feat: emailMessage 기본 생성자 추가

--- src/main/java/com/gg/tgather/commonservice/dto/mail/EmailMessage.java
+++ src/main/java/com/gg/tgather/commonservice/dto/mail/EmailMessage.java
@@ -1,10 +1,14 @@
 package com.gg.tgather.commonservice.dto.mail;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmailMessage {
 
     private String accountId;
